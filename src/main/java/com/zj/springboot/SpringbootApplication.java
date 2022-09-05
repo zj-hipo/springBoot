622ea3fa7cc2fc1dbe0007ddab102a3d4fa28c63
zj-hipo <542699293@qq.com>
2022-09-05 17:25:57 +0800

    2022-09-05

--- src/main/java/com/zj/springboot/SpringbootApplication.java
+++ src/main/java/com/zj/springboot/SpringbootApplication.java
@@ -10,12 +10,12 @@ public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		//熔断器接口配置
-		System.setProperty("csp.sentinel.dashboard.server","0.0.0.0:8082");
+		System.setProperty("csp.sentinel.dashboard.server","0.0.0.0:8081");
 		//熔断器应用名配置
 		System.setProperty("project.name","sentinel-demo");
 		SpringApplication.run(SpringbootApplication.class, args);
-		//启动jar包工具类
-		StartUpUtil.start("sentinel-dashboard-1.8.1.jar",null,true);
+		//启动jar包工具类(或者单独运行sentinel的jar包)
+		//StartUpUtil.start("sentinel-dashboard-1.8.1.jar",null,true);
 	}
 
 }
